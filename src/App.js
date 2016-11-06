8c5b91d3a0174a1a3b48c1ff7eb0e67bd0efe02f
annedien <annedient@gmail.com>
2016-11-06 10:13:09 +0100

    step2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+// src/App.js
 import React from 'react'
+import Title from './components/Title'
+import './App.sass'
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <Title label="Scoreboard" />
     )
   }
 }
